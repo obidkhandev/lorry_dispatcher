6f2241677dba06691e4e96a82f61541b66e566c3
o.abduraximov <obidxonf1234@gmail.com>
2025-08-20 16:49:09 +0500

    ..

---
+++ lib/features/profile/presentation/bloc/profile/profile_bloc.freezed.dart
@@ -0,0 +1,277 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'profile_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+/// @nodoc
+mixin _$ProfileState {
+
+ Status get getProfileStatus; ProfileResponseModel? get profile; Failure? get getProfileFailure;
+/// Create a copy of ProfileState
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$ProfileStateCopyWith<ProfileState> get copyWith => _$ProfileStateCopyWithImpl<ProfileState>(this as ProfileState, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is ProfileState&&(identical(other.getProfileStatus, getProfileStatus) || other.getProfileStatus == getProfileStatus)&&(identical(other.profile, profile) || other.profile == profile)&&(identical(other.getProfileFailure, getProfileFailure) || other.getProfileFailure == getProfileFailure));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,getProfileStatus,profile,getProfileFailure);
+
+@override
+String toString() {
+  return 'ProfileState(getProfileStatus: $getProfileStatus, profile: $profile, getProfileFailure: $getProfileFailure)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $ProfileStateCopyWith<$Res>  {
+  factory $ProfileStateCopyWith(ProfileState value, $Res Function(ProfileState) _then) = _$ProfileStateCopyWithImpl;
+@useResult
+$Res call({
+ Status getProfileStatus, ProfileResponseModel? profile, Failure? getProfileFailure
+});
+
+
+
+
+}
+/// @nodoc
+class _$ProfileStateCopyWithImpl<$Res>
+    implements $ProfileStateCopyWith<$Res> {
+  _$ProfileStateCopyWithImpl(this._self, this._then);
+
+  final ProfileState _self;
+  final $Res Function(ProfileState) _then;
+
+/// Create a copy of ProfileState
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? getProfileStatus = null,Object? profile = freezed,Object? getProfileFailure = freezed,}) {
+  return _then(_self.copyWith(
+getProfileStatus: null == getProfileStatus ? _self.getProfileStatus : getProfileStatus // ignore: cast_nullable_to_non_nullable
+as Status,profile: freezed == profile ? _self.profile : profile // ignore: cast_nullable_to_non_nullable
+as ProfileResponseModel?,getProfileFailure: freezed == getProfileFailure ? _self.getProfileFailure : getProfileFailure // ignore: cast_nullable_to_non_nullable
+as Failure?,
+  ));
+}
+
+}
+
+
+/// Adds pattern-matching-related methods to [ProfileState].
+extension ProfileStatePatterns on ProfileState {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _ProfileState value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _ProfileState() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _ProfileState value)  $default,){
+final _that = this;
+switch (_that) {
+case _ProfileState():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _ProfileState value)?  $default,){
+final _that = this;
+switch (_that) {
+case _ProfileState() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( Status getProfileStatus,  ProfileResponseModel? profile,  Failure? getProfileFailure)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _ProfileState() when $default != null:
+return $default(_that.getProfileStatus,_that.profile,_that.getProfileFailure);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( Status getProfileStatus,  ProfileResponseModel? profile,  Failure? getProfileFailure)  $default,) {final _that = this;
+switch (_that) {
+case _ProfileState():
+return $default(_that.getProfileStatus,_that.profile,_that.getProfileFailure);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( Status getProfileStatus,  ProfileResponseModel? profile,  Failure? getProfileFailure)?  $default,) {final _that = this;
+switch (_that) {
+case _ProfileState() when $default != null:
+return $default(_that.getProfileStatus,_that.profile,_that.getProfileFailure);case _:
+  return null;
+
+}
+}
+
+}
+
+/// @nodoc
+
+
+class _ProfileState implements ProfileState {
+  const _ProfileState({this.getProfileStatus = Status.UNKNOWN, this.profile, this.getProfileFailure = const UnknownFailure()});
+  
+
+@override@JsonKey() final  Status getProfileStatus;
+@override final  ProfileResponseModel? profile;
+@override@JsonKey() final  Failure? getProfileFailure;
+
+/// Create a copy of ProfileState
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$ProfileStateCopyWith<_ProfileState> get copyWith => __$ProfileStateCopyWithImpl<_ProfileState>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _ProfileState&&(identical(other.getProfileStatus, getProfileStatus) || other.getProfileStatus == getProfileStatus)&&(identical(other.profile, profile) || other.profile == profile)&&(identical(other.getProfileFailure, getProfileFailure) || other.getProfileFailure == getProfileFailure));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,getProfileStatus,profile,getProfileFailure);
+
+@override
+String toString() {
+  return 'ProfileState(getProfileStatus: $getProfileStatus, profile: $profile, getProfileFailure: $getProfileFailure)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$ProfileStateCopyWith<$Res> implements $ProfileStateCopyWith<$Res> {
+  factory _$ProfileStateCopyWith(_ProfileState value, $Res Function(_ProfileState) _then) = __$ProfileStateCopyWithImpl;
+@override @useResult
+$Res call({
+ Status getProfileStatus, ProfileResponseModel? profile, Failure? getProfileFailure
+});
+
+
+
+
+}
+/// @nodoc
+class __$ProfileStateCopyWithImpl<$Res>
+    implements _$ProfileStateCopyWith<$Res> {
+  __$ProfileStateCopyWithImpl(this._self, this._then);
+
+  final _ProfileState _self;
+  final $Res Function(_ProfileState) _then;
+
+/// Create a copy of ProfileState
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? getProfileStatus = null,Object? profile = freezed,Object? getProfileFailure = freezed,}) {
+  return _then(_ProfileState(
+getProfileStatus: null == getProfileStatus ? _self.getProfileStatus : getProfileStatus // ignore: cast_nullable_to_non_nullable
+as Status,profile: freezed == profile ? _self.profile : profile // ignore: cast_nullable_to_non_nullable
+as ProfileResponseModel?,getProfileFailure: freezed == getProfileFailure ? _self.getProfileFailure : getProfileFailure // ignore: cast_nullable_to_non_nullable
+as Failure?,
+  ));
+}
+
+
+}
+
+// dart format on
